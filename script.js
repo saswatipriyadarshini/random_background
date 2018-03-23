9c8d3aec69751d0a958055d522f2bc5142e9f67e
saswatipriyadarshini <saswati.p13@gmail.com>
2018-03-24 05:16:05 +0530

    Added a plain button

---
+++ script.js
@@ -0,0 +1,7 @@
+// function myFunction() {
+//     var letters = '0123456789ABCDEF'.split('');
+//     var color = '#';
+//     for (var i = 0; i < 6; i++ ) {color += letters[Math.round(Math.random() * 15)];
+//     }
+//     return color;
+// }
